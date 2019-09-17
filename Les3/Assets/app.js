cefc82f256ae674f4438a94198030089e16e9c11
Sander2002 <sanderveringa@gmail.com>
2019-09-17 11:12:59 +0200

    les3

---
+++ Les3/Assets/app.js
@@ -0,0 +1,17 @@
+let number = 4;
+number %= 2;
+if (number == 1) {
+    console.log("Oneven");
+} else {
+    console.log("Even");
+}
+
+
+let sentence = "Programming is not so cool";
+let sentence2 = sentence.replace("not ", "");
+console.log(sentence2);
+
+
+let number2 = 1400;
+let sentence3 = "Ik woon in Naboo"
+//Het is niet zinvol om ze te gaan vergelijken omdat de één een getal is en de ander een string (een zin) is.
